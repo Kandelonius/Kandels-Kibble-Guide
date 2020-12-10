931f5f81ddf26fbb6591a100c760ab4537fe0ea0
Shane Kaestner <shane.kaestner@yahoo.com>
2020-12-09 17:14:50 -0800

    cleaned up some of the default react app default stuff and started building out the basic app.

--- petapp/src/App.js
+++ petapp/src/App.js
@@ -1,13 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Kandel's kibbles
+          Kandel's Kibbles
         </p>
       </header>
     </div>
