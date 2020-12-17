1533463dc6c9240fdb33b7fd8dd117bfe36f3ee0
Shane Kaestner <shane.kaestner@yahoo.com>
2020-12-16 17:41:15 -0800

    update

--- petapp/src/components/common/Header.js
+++ petapp/src/components/common/Header.js
@@ -6,7 +6,9 @@ const Header = () => {
     return (
         <header className="navbar" style={{ backgroundColor: 'dodgerBlue' }}>
             <h1>Kandel's Kibbles</h1>
-            <NavLink to="/">Home</NavLink>
+            <div class="navLinks">
+                <NavLink to="/">Home</NavLink>
+            </div>
         </header>
     );
 };
