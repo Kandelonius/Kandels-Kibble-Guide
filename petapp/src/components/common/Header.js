015378d065ba4687414168785efbca4a6cbe9e5a
Shane Kaestner <shane.kaestner@yahoo.com>
2020-12-16 17:35:26 -0800

    updated the header background image and will use the other image in a different way.

--- petapp/src/components/common/Header.js
+++ petapp/src/components/common/Header.js
@@ -4,7 +4,7 @@ import pets from '../images/cat-and-dog-home.jpg';
 
 const Header = () => {
     return (
-        <header className="navbar" style={{ backgroundImage: `url(${pets})` }}>
+        <header className="navbar" style={{ backgroundColor: 'dodgerBlue' }}>
             <h1>Kandel's Kibbles</h1>
             <NavLink to="/">Home</NavLink>
         </header>
