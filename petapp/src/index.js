446c07a4719bdf77134e8879891b81376594ecb6
Shane Kaestner <shane.kaestner@yahoo.com>
2020-12-17 18:38:12 -0800

    changed the background color

--- petapp/src/index.js
+++ petapp/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, withRouter } from 'react-router-dom';
 import App from './App';
+import "./index.css";
 
 const AppWithRouter = withRouter(App);
 
