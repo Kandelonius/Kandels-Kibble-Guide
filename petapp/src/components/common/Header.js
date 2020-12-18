fbb9a25832eb8239aa0ce2d47ec7d5eb61dd856b
Shane Kaestner <shane.kaestner@yahoo.com>
2020-12-18 10:53:02 -0800

    have some styles completed with styled-components for the header.

--- petapp/src/components/common/Header.js
+++ petapp/src/components/common/Header.js
@@ -1,15 +1,18 @@
 import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import pets from '../images/cat-and-dog-home.jpg';
+import Div from './HeaderStiles.jsx';
 
 const Header = () => {
     return (
-        <header className="navbar" style={{ backgroundColor: '#8AA69F' }}>
-            <h1>Kandel's Kibbles</h1>
-            <div class="navLinks">
-                <NavLink to="/">Home</NavLink>
-            </div>
-        </header>
+        <Div>
+            <header className="navbar" style={{ backgroundColor: '#8AA69F' }}>
+                <h1>Kandel's Kibbles</h1>
+                <div class="navLinks">
+                    <NavLink to="/">Home</NavLink>
+                </div>
+            </header>
+        </Div>
     );
 };
 
---
+++ petapp/src/components/common/HeaderStiles.jsx
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+const Div = styled.div`
+    padding: 1%;
+    width:96%;
+    margin:0 auto;
+    header {
+        display:flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+        width:100%;
+        h1 {
+            margin-left: 5%;
+            text-align: left;
+        }
+        .navLinks {
+            margin-right: 5%;
+            text-align: right;
+            text-decoration: none;
+        }
+    }
+    
+`
+
+export default Div
