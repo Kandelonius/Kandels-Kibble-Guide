60e3ad88fd12d287ade6aeec0b234b625d97afce
Shane Kaestner <shane.kaestner@yahoo.com>
2020-12-19 01:19:46 -0800

    update

--- petapp/src/components/common/Header.js
+++ petapp/src/components/common/Header.js
@@ -10,6 +10,7 @@ const Header = () => {
                 <h1>Kandel's Kibbles</h1>
                 <div class="navLinks">
                     <NavLink to="/">Home</NavLink>
+                    <NavLink to="/">Home</NavLink>
                 </div>
             </header>
         </Div>
