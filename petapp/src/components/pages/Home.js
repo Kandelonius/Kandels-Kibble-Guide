adc5520ae43cf770ea10b53bfe1b1d95177d38ff
Shane Kaestner <shane.kaestner@yahoo.com>
2021-01-10 12:08:18 -0800

    added a quick lorem ipsum section for text on the home page.

--- petapp/src/components/pages/Home.js
+++ petapp/src/components/pages/Home.js
@@ -11,7 +11,9 @@ export default function Home() {
         <Div>
             <img src={petPic} alt="a cat and dog" />
             <div>
-
+                <p>
+                    Lorem ipsum odor amet, consectetuer adipiscing elit. Amet aptent tincidunt nisl ultrices tortor himenaeos placerat. Nulla odio enim, nam at phasellus quam. Ornare netus parturient in nostra lacinia. Orci morbi mollis lobortis elementum at sed. Dictum donec at ante morbi sodales. Placerat vel tristique diam venenatis placerat dapibus penatibus eleifend. Feugiat ut convallis volutpat dolor hac laoreet sem nibh dapibus. Auctor pellentesque viverra est pharetra nulla magnis orci sed mus.
+                </p>
             </div>
         </Div>
     )
