a31aaf80f598972a8bbdd2a86787b04a18f6dcc5
Shane Kaestner <shane.kaestner@yahoo.com>
2021-01-22 16:59:11 -0800

    added structure to the pet model.

--- pet-back-end/src/main/java/com/kibbles/app/models/Userpet.java
+++ pet-back-end/src/main/java/com/kibbles/app/models/Userpet.java
@@ -1,10 +1,51 @@
 package com.kibbles.app.models;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
 
 @Entity
 @Table(name = "userpet")
 
 public class Userpet {
+
+    /**
+     * The primary key (long) of the userpet table
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long petid;
+
+    @ManyToOne
+    @JoinColumn(name = "userid",
+        nullable = false)
+    @JsonIgnoreProperties(value = "userpet",
+        allowSetters = true)
+    private User user;
+
+    public Userpet() {
+    }
+
+    public Userpet(
+        long petid,
+        User user) {
+        this.petid = petid;
+        this.user = user;
+    }
+
+    public long getPetid() {
+        return petid;
+    }
+
+    public void setPetid(long petid) {
+        this.petid = petid;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
