1d3be587ca1db1f77e23080ef97f8c50f258b426
Shane Kaestner <shane.kaestner@yahoo.com>
2021-03-15 16:28:00 -0700

    updated pet service impl

--- pet-back-end/src/main/java/com/kibbles/app/services/PetServiceImpl.java
+++ pet-back-end/src/main/java/com/kibbles/app/services/PetServiceImpl.java
@@ -4,6 +4,9 @@ import com.kibbles.app.models.Pet;
 import com.kibbles.app.repository.PetRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import javax.persistence.EntityExistsException;
+import javax.persistence.EntityNotFoundException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class PetServiceImpl implements PetService{
@@ -13,26 +16,43 @@ public class PetServiceImpl implements PetService{
 
     @Override
     public List<Pet> findAll() {
-        return null;
+        List<Pet> list = new ArrayList<>();
+        /*
+         * findAll returns an iterator set.
+         * iterate over the iterator set and add each element to an array list.
+         */
+        petrepos.findAll()
+            .iterator()
+            .forEachRemaining(list::add);
+        return list;
     }
 
     @Override
     public Pet findPetById(long id) {
-        return null;
+        return petrepos.findById(id)
+            .orElseThrow(() -> new EntityNotFoundException("Pet id " + id + " not found!"));
     }
 
     @Override
     public Pet save(Pet pet) {
-        return null;
+        if (pet.getUser()
+            .size() > 0)
+        {
+            throw new EntityExistsException("Pet already exists.");
+        }
+
+        return petrepos.save(pet);
     }
 
     @Override
     public void delete(long id) {
-
+        petrepos.findById(id)
+            .orElseThrow(() -> new EntityNotFoundException("Pet id " + id + " not found!"));
+        petrepos.deleteById(id);
     }
 
     @Override
     public void deleteAll() {
-
+        petrepos.deleteAll();
     }
 }
