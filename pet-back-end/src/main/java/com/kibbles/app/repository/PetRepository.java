030800bc552fe78ad46947206a49748ba0ca5627
Shane Kaestner <shane.kaestner@yahoo.com>
2022-12-27 19:38:58 -0800

    restructure to Angular / .Net app

--- pet-back-end/src/main/java/com/kibbles/app/repository/PetRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kibbles.app.repository;
-
-import com.kibbles.app.models.Pet;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PetRepository
-    extends CrudRepository<Pet, Long> {
-
-}
