e506a9ac7ffe9001632181d572570585636c8599
Shane Kaestner <shane.kaestner@yahoo.com>
2023-01-01 18:09:21 -0800

    created a basic form for users in the front end.

--- petapp/KibbleFrontend/src/app/user-form/user-form.component.ts
+++ petapp/KibbleFrontend/src/app/user-form/user-form.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user-form',
   templateUrl: './user-form.component.html',
-  styleUrls: ['./user-form.component.scss']
+  styleUrls: ['./user-form.component.scss'],
 })
-export class UserFormComponent {
+export class UserFormComponent implements OnInit {
+  validateForm!: FormGroup;
 
+  submitForm(value: { firstName: string; completed: boolean }): void {
+    for (const key in this.validateForm.controls) {
+      if (this.validateForm.controls.hasOwnProperty(key)) {
+        this.validateForm.controls[key].markAsDirty();
+        this.validateForm.controls[key].updateValueAndValidity();
+      }
+    }
+    value.completed = false;
+    this.validateForm.reset();
+  }
+  /**
+   *
+   */
+  constructor(private fb: FormBuilder) {}
+  ngOnInit(): void {
+    this.validateForm = this.fb.group({
+      firstName: [null, [Validators.required]],
+    });
+  }
 }
